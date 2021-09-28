26d0008553b651d37833640e33b2ec9df1f078b1
Vaishnavi0522 <91020838+Vaishnavi0522@users.noreply.github.com>
2021-09-28 13:14:25 +0530

    Add files via upload

---
+++ pythonexe1.py
@@ -0,0 +1,9 @@
+a=10
+b=90
+c=20
+if(a>b):
+    print("a is bigger")
+elif(b>c):
+    print("b is bigger")
+else:
+    print("c is bigger")
---
+++ pythonexe3.py
@@ -0,0 +1,5 @@
+name1=str(input("Enter your name:"))
+num =int(input("Enter your age"))
+num1=100-num
+print (num1)
+
